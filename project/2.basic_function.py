f32bdb03626a179903c388c37f6ec9673b6f055d
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-09-15 18:16:29 +0900

    사진 합치기 프로젝트, 파일 정리
    파일 선택 & 파일 삭제 기능 추가

---
+++ project/2.basic_function.py
@@ -0,0 +1,111 @@
+from tkinter import *
+import tkinter.ttk as ttk   # __all__
+from tkinter import filedialog  # 서브 모듈이기 때문에 별도로 선언
+
+root = Tk()
+root.title("project")
+
+# 파일 추가
+def add_file():
+    files = filedialog.askopenfilenames(title= "이미지 파일을 선택하세요", \
+                                        filetypes = (("PNG 파일", "*.png"),("모든 파일", "*.*")),\
+                                        initialdir = r"C:\Users\USER\Pictures\Camera Roll") # C:/ 보여줌
+    # 선택한 파일 목록
+    for file in files:
+        list_file.insert(END,file)
+
+# 선택 삭제
+def del_file():
+    for index in reversed(list_file.curselection()):
+        list_file.delete(index)
+
+# 파일 프레임 (파일 추가, 선택 삭제)
+file_frame = Frame(root)
+file_frame.pack(fill = 'x', padx = 5, pady =5)
+
+btn_add_file = Button(file_frame, text = "파일추가", padx = 5, width = 12, pady = 5, command = add_file)
+btn_add_file.pack(side = 'left')
+
+btn_del_file = Button(file_frame, text = '선택삭제', padx = 5, width = 12, pady = 5, command = del_file)
+btn_del_file.pack(side = "right")
+
+# 리스트 프레임
+list_frame = Frame(root)
+list_frame.pack(fill = 'both', padx = 5, pady =5)
+
+scrollbar = Scrollbar(list_frame)
+scrollbar.pack(side = 'right', fill = 'y')
+
+list_file = Listbox(list_frame, selectmode = "extended", height = 15, yscrollcommand = scrollbar.set)
+list_file.pack(side = 'left', fill= 'both', expand = True)
+scrollbar.config(command = list_file.yview())
+
+# 저장 경로 프레임
+path_frame = LabelFrame(root, text = '저장경로')
+path_frame.pack(fill = 'x', padx = 5, pady =5, ipady = 5)
+
+txt_dest_path = Entry(path_frame)
+txt_dest_path.pack(side = 'left', fill = 'x', expand = True, ipady = 4, padx = 5, pady =5)  # 높이 변경
+
+btn_dest_path = Button(path_frame, text = '찾아보기', width = 10)
+btn_dest_path.pack(side = 'right', padx = 5, pady =5)
+
+# 옵션 프레임
+frame_option = LabelFrame(root, text = "옵션")
+frame_option.pack(padx = 5, pady =5, ipady = 5)
+
+# 1 가로 넓이 옵션
+# 가로 넒이 레이블
+lbl_width = Label(frame_option, text = "가로 넓이", width = 8)
+lbl_width.pack(side = "left", padx = 5, pady =5)
+
+# 가로 넓이 콤보
+opt_width = ['원본 유지', '1024','800','640']
+cmb_width = ttk.Combobox(frame_option,state = 'readonly', values = opt_width, width = 10)
+cmb_width.current(0)
+cmb_width.pack(side = 'left', padx = 5, pady =5)
+
+# 2. 간격 옵션 레이블
+# 간격 옵션 레이블
+lbl_space = Label(frame_option, text = '간격', width = 8)
+lbl_space.pack(side = 'left')
+
+# 간격 옵션 콤보
+opt_space = ['없음','좁게','보통', '넓게']
+cmb_space = ttk.Combobox(frame_option,state = 'readonly', values = opt_space, width = 10)
+cmb_space.current(0)
+cmb_space.pack(side = 'left', padx = 5, pady =5)
+
+# 3. 파일 포맷 옵션
+# 파일 포맷 옵션 레이블
+lbl_format = Label(frame_option,text = '포맷',width = 8)
+lbl_format.pack(side = 'left', padx = 5, pady =5)
+
+# 파일 포맷 옵션 콤보
+opt_format = ['PNG', 'JPG',"BMP"]
+cmb_format = ttk.Combobox(frame_option, state = 'readonly', values = opt_format, width = 10)
+cmb_format.current(0)
+cmb_format.pack(side = 'left', padx = 5, pady =5)
+
+# 진행 상황 Progress Bar
+frame_progress = LabelFrame(root, text = '진행상황')
+frame_progress.pack(fill = 'x', padx = 5, pady =5)
+
+p_var = DoubleVar()
+progressbar = ttk.Progressbar(frame_progress, maximum = 100, variable = p_var)
+progressbar.pack(fill = 'x', padx = 5, pady =5, ipady = 5)
+
+# 실행 프레임
+frame_run = Frame(root)
+frame_run.pack(fill = 'x', padx = 5, pady =5)
+
+btn_close = Button(frame_run, padx = 5 ,pady = 5, text = '닫기', width = 12, command = root.quit)
+btn_close.pack(side = 'right', padx = 5, pady =5)
+
+btn_start = Button(frame_run, padx = 5, pady = 5, text = '시작', width = 12)
+btn_start.pack(side = 'right', padx = 5, pady =5)
+
+
+
+root.resizable(False,False)
+root.mainloop()
