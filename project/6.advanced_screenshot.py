200cfe8f8f95a479cac6782b5730c0ccfe7388a0
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-09-16 19:08:56 +0900

    스크린 샷
    키보드 특정 키를 누를 때마다 캡쳐

---
+++ project/6.advanced_screenshot.py
@@ -0,0 +1,12 @@
+import keyboard
+from PIL import ImageGrab
+import time
+
+def screenshot():
+    curr_time = time.strftime("_%Y%m%d_%H%M%S")
+    img = ImageGrab.grab()
+    img.save("image{}.png".format(curr_time))
+
+keyboard.add_hotkey("F9", screenshot)   # f9 누르면 캡쳐
+
+keyboard.wait("esc") # esc 누를 때까지 실행
