721460d93dd101080d8e42ccc59637ae5e982c44
dmschd0678@naver.com <63108606+dmschd0678@users.noreply.github.com>
2021-10-06 19:44:19 +0900

    Update 8.combobox.py

--- GUI/8.combobox.py
+++ GUI/8.combobox.py
@@ -7,7 +7,7 @@ root.title("tkinter GUI")
 values = [str(i) + "일" for i in range(1,32)]
 
 # 선택 창에 글을 쓸 수가 있음
-combobox = ttk.Combobox(root, height = 5, values = values)  # height = ? : ? 의 갯수만큼 값을 보여줌
+combobox = ttk.Combobox(root, height = 5, values = values)  # height = ? , ? 의 갯수만큼 값을 보여줌
 combobox.pack()
 combobox.set("카드 결제일") #최초 목록 제목 설정, 버튼 클릭을 통한 설정
 
